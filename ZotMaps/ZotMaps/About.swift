f52a471054141f43a1fe8eb9be8aa58547cb696d
David Liu <sdavidliu@gmail.com>
2017-09-14 19:28:00 -0700

    Version 1.3

--- ZotMaps/ZotMaps/About.swift
+++ ZotMaps/ZotMaps/About.swift
@@ -12,11 +12,32 @@ class About: UIViewController {
 
     @IBOutlet weak var label1: UILabel!
     @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         label1.sizeToFit()
         label2.sizeToFit()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(About.rotated), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+
+    }
+    
+    func rotated() {
+        if UIDeviceOrientationIsLandscape(UIDevice.current.orientation) {
+            print("Landscape")
+            imageView.frame.size.width = 120
+            imageView.frame.size.height = 120
+            imageView.center = CGPoint(x: UIScreen.main.bounds.width/2, y: imageView.center.y)
+        }
+        
+        if UIDeviceOrientationIsPortrait(UIDevice.current.orientation) {
+            print("Portrait")
+            imageView.frame.size.width = 180
+            imageView.frame.size.height = 180
+            imageView.center = CGPoint(x: UIScreen.main.bounds.width/2, y: imageView.center.y)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
